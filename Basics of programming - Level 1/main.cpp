10630866f37c82d6d45cdfa3be43733f52f2afb0
kunalahirwar2001 <kunalahirwar2001@gmail.com>
2025-01-07 11:44:52 +0530

    add comments to main.cpp for better understanding of code functionality

--- Basics of programming - Level 1/main.cpp
+++ Basics of programming - Level 1/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-    // int main = 10;
-    // cout << main;
-    // cout << "hello world";
+// #include <iostream> means we're using the iostream library, which allows us to perform input and output operations.
+// #include <cmath> means we're using the cmath library, which allows us to perform mathematical operations.
 
-    // age store karne ke liye memory chahiye
-    int age;
-    // cout << age;
-    cout << "Enter your age sanjana" << endl;
-    cin >> age;
-    cout << "Your age is: " << age << endl;
+using namespace std;
+// This line tells the compiler to use the standard namespace, which allows us to use standard library functions without specifying the namespace.
+// For example, we can use cout instead of std::cout.
+// The standard namespace contains functions and objects that are part of the C++ standard library.
+// some of the standard functions are cin, cout, endl, etc.
+//some namespace are std, std::chrono, std::filesystem, std::literals, std::placeholders, std::rel_ops, std::this_thread, std::chrono_literals, std::filesystem_literals, std::placeholders_literals, std::rel_ops_literals, std::this_thread_literals.
+
+int main(int argc, char const *argv[])
+// The main function is the entry point of the program.
+// int argc, char const *argv[] are command line arguments that can be passed to the program.
+{
+    cout<<"Hello, World!"<<endl;
+    // This line prints "Hello, World!" to the console.
+
     return 0;
+    // This line indicates that the program has executed successfully and returns 0 to the operating system.
 }
