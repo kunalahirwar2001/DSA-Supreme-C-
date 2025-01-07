2a289440dc0028596f08a0990fb0d9c7e0ba0fff
kunalahirwar2001 <kunalahirwar2001@gmail.com>
2025-01-07 09:26:57 +0530

    add basic age input program in main.cpp

---
+++ Basics of programming - Level 1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    // int main = 10;
+    // cout << main;
+    // cout << "hello world";
+
+    // age store karne ke liye memory chahiye
+    int age;
+    // cout << age;
+    cout << "Enter your age sanjana" << endl;
+    cin >> age;
+    cout << "Your age is: " << age << endl;
+    return 0;
+}
